cba8c3631d8e5b09755ed311011ab331969373a5
Khushi-Lalchandani <140065096+Khushi-Lalchandani@users.noreply.github.com>
2024-10-04 11:40:51 +0530

    Updated

--- src/app/edit-overlay/edit-overlay.component.ts
+++ src/app/edit-overlay/edit-overlay.component.ts
@@ -8,12 +8,18 @@ import { Image } from '../format.model';
   styleUrls: ['./edit-overlay.component.scss'],
 })
 export class EditOverlayComponent implements OnInit {
-  tags!: [];
+  tags!: string;
   @Output() showEdit = new EventEmitter<boolean>();
   @Input() select!: Image[];
   constructor(private imgService: ImageService) {}
   ngOnInit(): void {}
   onEmit() {
     this.showEdit.emit(false);
+  }
+  onInput($event: KeyboardEvent) {
+    if ($event.key === 'Enter') {
+      this.select[0].tag.push(this.tags);
+      console.log(this.select, this.tags);
+    }
   }
 }
