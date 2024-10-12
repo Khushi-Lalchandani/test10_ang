7e3fef17d5d5c1f5abff0cdc546316fddb6ddd38
Khushi-Lalchandani <140065096+Khushi-Lalchandani@users.noreply.github.com>
2024-10-12 17:42:57 +0530

    Updated

--- src/app/overlay/overlay.component.ts
+++ src/app/overlay/overlay.component.ts
@@ -16,14 +16,30 @@ export class OverlayComponent implements OnInit {
     this.imgService.getImages().subscribe((images) => {
       this.imageDetails = images;
     });
+
+    this.imgService.detectChange.subscribe((changed) => {
+      if (changed) {
+        this.uploadChanges();
+      }
+    });
+  }
+
+  uploadChanges() {
+    this.imgService.updateImageDetails(this.imageDetails).subscribe(
+      (response) => {
+        console.log('Updated successfully', response);
+      },
+      (error) => {
+        console.error('Error updating data', error);
+      }
+    );
   }
   deleteImage(url: any) {
     this.show.emit(false);
     this.imgService.deleteImage(url);
     this.imageDetails = [...this.imgService.originalImageDetails];
-    this.imgService.updateImageDetails(this.imageDetails).subscribe(() => {
-      this.emitDetails.emit(this.imageDetails);
-    });
+    this.imgService.detectChange.next(true);
+    this.emitDetails.emit(this.imageDetails);
   }
 
   onEmit() {
